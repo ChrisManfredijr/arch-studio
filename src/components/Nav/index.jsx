4f14fb8392d9b7f5af32136fa91249f9aaa769e2
Chris <chrismanfredijr@gmail.com>
2023-01-30 21:57:59 -0500

    tablet navbar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,9 +2,14 @@ import {ReactComponent as Logo} from '../../assets/logo.svg';
 import {ReactComponent as Hamburger} from '../../assets/icons/icon-hamburger.svg';
 const Nav = () => {
   return (
-    <div className="h-[96px] p-[32px] pl-[12px] flex justify-between">
-      <Logo className="scale-75"/>
-      <Hamburger className="my-auto"/>
+    <div className="h-[96px] p-[32px] pl-[12px] flex justify-between md:justify-start md:px-0 md:w-3/4 md:mx-auto md:my-6">
+      <Logo className="scale-75 md:scale-100"/>
+      <ul className='hidden text-medium-grey font-bold md:flex md:leading-9 md:text-xl'>
+        <li className='md:pl-16'>Portfolio</li>
+        <li className='md:pl-16'>About Us</li>
+        <li className='md:pl-16'>Contact</li>
+      </ul>
+      <Hamburger className="my-auto md:hidden"/>
     </div>
   )
 }
