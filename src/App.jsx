d45886c3a2e2b3acbd2e3ff4b4bd87ccbf58f097
Chris <chrismanfredijr@gmail.com>
2023-01-30 21:24:22 -0500

    intial mobile navbar"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home'; 
 import Footer from './components/Footer';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="h-screen w-screen">
+        <Nav/>
         <Home/>
         <Footer/>
     </div>
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,7 +1,11 @@
-
+import {ReactComponent as Logo} from '../../assets/logo.svg';
+import {ReactComponent as Hamburger} from '../../assets/icons/icon-hamburger.svg';
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className="h-[96px] p-[32px] pl-[12px] flex justify-between">
+      <Logo className="scale-75"/>
+      <Hamburger className="my-auto"/>
+    </div>
   )
 }
 
